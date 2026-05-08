bb191bb892c392399b872570dd5cf6c842fb6de5
ivan zablotski <vanyazablotski012@gmail.com>
2026-05-08 14:18:39 +0300

    класи картина, об'ємний експонат

--- kursach/classes/picture.cs
+++ kursach/classes/picture.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace kursach.classes
 {
-    public class picture
+    public class Picture : Exhibit
     {
+        private int height;
+        private int width;
+
+        public Picture(string author, string country, int year, int height, int width) : base(author, country, year)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { 
+                if (value < 0) {    
+                    throw new ArgumentException("Висота не може бути від'ємною.");
+                }
+                height = value;
+            }
+        }
+        public int Width
+        {
+            get { return width; }
+            set { 
+                if (value < 0) {    
+                    throw new ArgumentException("Ширина не може бути від'ємною.");
+                }
+                width = value;
+            }
+        }
+         public override string ToString()
+        {
+            return $"Картина: автор = {Author}, країна = {Country}, рік = {Year}, висота = {Height}, ширина = {Width}";
+        }
     }
 }
--- kursach/classes/volumeexhibit.cs
+++ kursach/classes/volumeexhibit.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace kursach.classes
 {
-    public class VolumeExhibit
+    public class VolumeExhibit : Exhibit
     {
+        private int width;
+        private int length;
+        private int height;
+        public VolumeExhibit(string author, string country, int year, int width, int length, int height) : base(author, country, year)
+        {
+            Width = width;
+            Length = length;
+            Height = height;
+        }
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Ширина не може бути від'ємною.");
+                width = value;
+            }
+        }
+        public int Length
+        {
+            get { return length; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Довжина не може бути від'ємною.");
+                length = value;
+            }
+        }
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Висота не може бути від'ємною.");
+                height = value;
+            }
+        }
+         public override string ToString()
+        {
+            return $"Об'ємний експонат: автор = {Author}, країна = {Country}, рік = {Year}, ширина = {Width}, довжина = {Length}, висота = {Height}";
+        }
     }
 }
